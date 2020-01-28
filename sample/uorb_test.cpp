273aa71905cf37f1c373f9f83c1a7699ec403c96
FengShun <fengshun95@gmail.com>
2020-01-28 22:58:47 +0800

    Optimize test routines

--- sample/uorb_test.cpp
+++ sample/uorb_test.cpp
@@ -6,9 +6,10 @@
 #include <pthread.h>
 
 #include "../base/drv_hrt.h"
+#include "../base/orb_defines.h"
 #include "../base/orb_posix.h"
-#include "ulog/src/ulog.h"
 #include "topic_header/cpuload.h"
+#include "ulog/src/ulog.h"
 
 void *adviser_cpuload(void *) {
   struct cpuload_s cpuload {};
@@ -18,6 +19,12 @@ void *adviser_cpuload(void *) {
   cpuload.ram_usage = 1.0f;
 
   orb_advert_t cpuload_pub = orb_advertise_queue(ORB_ID(cpuload), &cpuload, 10);
+  if (cpuload_pub == nullptr) {
+    LOG_WARN("advertise error");
+    return nullptr;
+  } else {
+    LOG_INFO("advertise successful");
+  }
 
   usleep(2 * 1000 * 1000);
   for (int i = 0; i < 10; i++) {
@@ -33,10 +40,16 @@ void *adviser_cpuload(void *) {
 void* cpuload_update_poll(void* arg) {
   struct cpuload_s cpu_loader {};
   int cpuload_sub = orb_subscribe(ORB_ID(cpuload));
+  if (cpuload_sub == ORB_ERROR) {
+    LOG_ERROR("orb_subcribe error: %s", strerror(orb_errno));
+    return nullptr;
+  }
 
   uint32_t sleep_time_us = 0;
   if (arg)
     sleep_time_us = *(uint32_t *) arg;
+
+  LOG_INFO("orb_subcribe success, cycle: %.1fs", sleep_time_us / 1000.f / 1000.f);
 
   orb_pollfd_struct_t fds[] = {
       {.fd = cpuload_sub, .events = POLLIN},
@@ -70,17 +83,15 @@ void* cpuload_update_poll(void* arg) {
 
 void uorb_sample() {
   // One publishing thread, three subscription threads
-  pthread_t pthread1, pthread2, pthread3, pthread4;
-  pthread_create(&pthread1, nullptr, adviser_cpuload, nullptr);
+  pthread_t pthread_id;
+  pthread_create(&pthread_id, nullptr, adviser_cpuload, nullptr);
   static uint32_t sleep_time_us_1 = 1 * 1000 * 1000;
-  pthread_create(&pthread2, nullptr, cpuload_update_poll, &sleep_time_us_1);
+  pthread_create(&pthread_id, nullptr, cpuload_update_poll, &sleep_time_us_1);
   static uint32_t sleep_time_us_2 = 1.5 * 1000 * 1000;
-  pthread_create(&pthread3, nullptr, cpuload_update_poll, &sleep_time_us_2);
+  pthread_create(&pthread_id, nullptr, cpuload_update_poll, &sleep_time_us_2);
   static uint32_t sleep_time_us_3 = 2 * 1000 * 1000;
-  pthread_create(&pthread4, nullptr, cpuload_update_poll, &sleep_time_us_3);
+  pthread_create(&pthread_id, nullptr, cpuload_update_poll, &sleep_time_us_3);
 }
-
-int uorb_main(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
   uorb_sample();
