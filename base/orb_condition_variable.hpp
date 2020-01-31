396e0aaf47cb168ec95bf11b11b1c4b5ff178e3c
FengShun <fengshun95@gmail.com>
2020-01-31 20:29:45 +0800

    WIP: Returns true if the condition variable waits for success

--- base/orb_condition_variable.hpp
+++ base/orb_condition_variable.hpp
@@ -25,42 +25,47 @@ public:
   ConditionVariable(const ConditionVariable &) = delete;
   ConditionVariable &operator=(const ConditionVariable &) = delete;
 
-  int notify_one() noexcept { return pthread_cond_signal(&cond_); }
+  void notify_one() noexcept { pthread_cond_signal(&cond_); }
 
-  int notify_all() noexcept { return pthread_cond_broadcast(&cond_); }
+  void notify_all() noexcept { pthread_cond_broadcast(&cond_); }
 
-  int wait(Mutex &lock) noexcept {
-    return pthread_cond_wait(&cond_, lock.native_handle());
+  void wait(Mutex &lock) noexcept {
+    pthread_cond_wait(&cond_, lock.native_handle());
   }
 
   template <typename _Predicate>
-  int wait(Mutex &lock, _Predicate p) {
-    int ret = 0;
-    while (!p()) ret = wait(lock);
-    return ret;
+  void wait(Mutex &lock, _Predicate p) {
+    while (!p()) wait(lock);
   }
 
-  int wait_until(Mutex &lock, const struct timespec &atime) {
-    return pthread_cond_timedwait(&cond_, lock.native_handle(), &atime);
+  // Return true if successful
+  bool wait_until(Mutex &lock, const struct timespec &atime) {
+    return pthread_cond_timedwait(&cond_, lock.native_handle(), &atime) == 0;
   }
 
-  int wait_until(Mutex &lock, const struct timespec *atime) {
-    return pthread_cond_timedwait(&cond_, lock.native_handle(), atime);
-  }
-
+  // Return true if successful
   template <typename _Predicate>
   bool wait_until(Mutex &lock, const struct timespec &atime, _Predicate p) {
     while (!p())
-      if (wait_until(lock, atime) == ETIMEDOUT) return p();
+      if (!wait_until(lock, atime)) return p();
     return true;
   }
 
-  int wait_for(Mutex &lock, long usec) {
+  // Return true if successful
+  bool wait_for(Mutex &lock, long usec) {
     struct timespec req {};
     clock_gettime(clock_id, &req);
     req.tv_nsec += usec * 1000;
     req.tv_sec += req.tv_nsec / (1e9);
     return wait_until(lock, req);
+  }
+
+  // Return true if successful
+  template <typename _Predicate>
+  bool wait_for(Mutex &lock, long usec, _Predicate p) {
+    while (!p())
+      if (!wait_for(lock, usec)) return p();
+    return true;
   }
 
   pthread_cond_t *native_handle() { return &cond_; }
