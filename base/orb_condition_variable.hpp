8d06280bc0ad9a3fddb8e9e725acf19bc18858b4
FengShun <fengshun95@gmail.com>
2020-01-31 21:57:25 +0800

    fix: wait_for function needs to return after timeout

--- base/orb_condition_variable.hpp
+++ base/orb_condition_variable.hpp
@@ -1,17 +1,16 @@
 #pragma once
 
 #include <pthread.h>
+#include <time.h>
 
 #include "orb_errno.h"
 #include "orb_mutex.hpp"
 
 namespace uORB {
 
-template <int clock_id>
+template <clockid_t clock_id>
 class ConditionVariable {
-  pthread_cond_t cond_{};
-
-public:
+ public:
   ConditionVariable() noexcept {
     pthread_condattr_t attr;
     pthread_condattr_init(&attr);
@@ -46,29 +45,45 @@ public:
   // Return true if successful
   template <typename _Predicate>
   bool wait_until(Mutex &lock, const struct timespec &atime, _Predicate p) {
+    // Not returned until timeout or other error
     while (!p())
       if (!wait_until(lock, atime)) return p();
     return true;
   }
 
   // Return true if successful
-  bool wait_for(Mutex &lock, long usec) {
-    struct timespec req {};
-    clock_gettime(clock_id, &req);
-    req.tv_nsec += usec * 1000;
-    req.tv_sec += req.tv_nsec / (1e9);
-    return wait_until(lock, req);
+  bool wait_for(Mutex &lock, unsigned long time_ms) {
+    struct timespec atime {};
+    GenerateFutureTime(clock_id, time_ms, atime);
+    return wait_until(lock, atime);
   }
 
   // Return true if successful
   template <typename _Predicate>
-  bool wait_for(Mutex &lock, long usec, _Predicate p) {
+  bool wait_for(Mutex &lock, unsigned long time_ms, _Predicate p) {
+    struct timespec atime {};
+    GenerateFutureTime(clock_id, time_ms, atime);
+
+    // Not returned until timeout or other error
     while (!p())
-      if (!wait_for(lock, usec)) return p();
+      if (!wait_until(lock, atime)) return p();
     return true;
   }
 
   pthread_cond_t *native_handle() { return &cond_; }
+
+ private:
+  // Increase time_ms time based on the current clockid time
+  static void GenerateFutureTime(clockid_t clockid, unsigned long time_ms,
+                                 struct timespec &out) {
+    const decltype(out.tv_nsec) kSec2Nsec = 1000 * 1000 * 1000;
+    clock_gettime(clockid, &out);
+    out.tv_nsec += time_ms * 1000 * 1000;
+    out.tv_sec += out.tv_nsec / kSec2Nsec;
+    out.tv_nsec %= kSec2Nsec;
+  }
+
+  pthread_cond_t cond_{};
 };
 
 typedef ConditionVariable<CLOCK_MONOTONIC> MonoClockCond;
