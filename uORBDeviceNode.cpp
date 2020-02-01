42335bd4276abb835d22388dcd9fd48a392bd261
FengShun <fengshun95@gmail.com>
2020-02-02 07:38:59 +0800

    Fix: Keep header file declaration consistent with source file definition
    Remove invalid const modifications

--- uORBDeviceNode.cpp
+++ uORBDeviceNode.cpp
@@ -56,7 +56,7 @@ uORB::DeviceNode::SubscriberData *uORB::DeviceNode::filp_to_sd(
 }
 
 uORB::DeviceNode::DeviceNode(const struct orb_metadata *meta,
-                             const uint8_t instance, const char *path,
+                             uint8_t instance, const char *path,
                              uint8_t priority, uint8_t queue_size)
     : _devname(path),
       _meta(meta),
