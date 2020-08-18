f7d0853204fef2c988607887f616cdf469d82317
FengShun <fengshun95@gmail.com>
2020-08-18 17:32:20 +0800

    Add build and install script

--- build.sh
+++ build.sh
@@ -1,17 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
+SOURCE_DIR=$(
+  cd "$(dirname $0)" || exit
+  pwd
+)
+
+# Get the platform name from the first parameter
+if [ $# -ge 1 ]; then
+  PLATFORM_NAME=$1
+else
+  echo "USAGE: $0 [platform_name]"
+fi
+
+# Get platform toolchain info
+PLATFORM_NAME=${PLATFORM_NAME:-host.gcc}
+PLATFORM_TOOLCHAIN_FILE=$SOURCE_DIR/toolchains/$PLATFORM_NAME.toolchain.cmake
+
+# Check if the platform toolchain exists
+if [ ! -f $PLATFORM_TOOLCHAIN_FILE ]; then
+  echo "Can't find platform toolchain file: $PLATFORM_TOOLCHAIN_FILE"
+  exit 1
+fi
+
+# Build library
 set -x
-
-SOURCE_DIR=$(cd "$(dirname "$0")" || exit;pwd)
-BUILD_TYPE=${BUILD_TYPE:-release}
-BUILD_DIR=${BUILD_DIR:-cmake-build-${BUILD_TYPE}}
-INSTALL_DIR=${INSTALL_DIR:-install}
-
-mkdir -p $BUILD_DIR \
-  && cd $BUILD_DIR \
-  && cmake \
-           -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-           -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-           -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-           $SOURCE_DIR \
-  && make $*
+mkdir -p "$SOURCE_DIR/cmake-build-$PLATFORM_NAME" &&
+  cd "$SOURCE_DIR/cmake-build-$PLATFORM_NAME" &&
+  cmake \
+    -DCMAKE_TOOLCHAIN_FILE="$PLATFORM_TOOLCHAIN_FILE" \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    .. &&
+  make -j4 &&
+  make install
