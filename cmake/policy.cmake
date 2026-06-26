f323a352de62425b2f0d8ba80041450231bf5ded
Shawn Feng <shawnfeng0@gmail.com>
2026-06-26 13:51:47 +0800

    build: make CMake compatible with CMake 3.5
    - Lower cmake_minimum_required from 3.10 to 3.5
    - Replace VERSION_GREATER_EQUAL (introduced in 3.7) with NOT VERSION_LESS
    - Replace target_compile_features(cxx_std_11) (requires 3.8+)
      with set(CMAKE_CXX_STANDARD 11)
    - Comment out install() rules for embedded builds

--- cmake/policy.cmake
+++ cmake/policy.cmake
@@ -4,7 +4,7 @@
 # http://www.cmake.org/cmake/help/v3.0/command/cmake_policy.html
 macro (use_cmake_policy)
     # Avoid warning about DOWNLOAD_EXTRACT_TIMESTAMP in CMake 3.24:
-    if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
+    if (NOT CMAKE_VERSION VERSION_LESS "3.24.0")
         cmake_policy(SET CMP0135 NEW)
     endif()
 endmacro()
